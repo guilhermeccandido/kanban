af895babad019c3f4a86bb68a60ef6ba614131ff
momingse <da1710080850@gmail.com>
2024-01-15 10:44:33 +0800

    change font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,29 @@
-import AppLayout from '@/components/AppLayout';
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { Providers } from '@/components/Provider';
-import { Toaster } from '@/components/ui/toaster';
+import AppLayout from "@/components/AppLayout";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Providers } from "@/components/Provider";
+import { Toaster } from "@/components/ui/toaster";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-	title: 'Simple Todo',
-	description: 'A simple todo site',
+  title: "Simple Todo",
+  description: "A simple todo site",
 };
 
 export default function RootLayout({ children }: { children: JSX.Element }) {
-	return (
-		<html lang='en'>
-			<body className={inter.className}>
-				<Providers>
-					<AppLayout>{children}</AppLayout>
-				</Providers>
-				<Toaster />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={roboto.className}>
+        <Providers>
+          <AppLayout>{children}</AppLayout>
+        </Providers>
+        <Toaster />
+      </body>
+    </html>
+  );
 }
