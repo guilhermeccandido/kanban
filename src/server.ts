9359b8ae359d7689ec8ce2a9536eab8d9e7688ed
momingse <da1710080850@gmail.com>
2024-07-28 16:02:34 +0800

    refactor: run tools of ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,23 @@
-const cli = require('next/dist/cli/next-dev')
-const dotenv = require('dotenv')
+import next from "next";
+import dotenv from "dotenv";
+import { createServer } from "node:http";
 
-dotenv.config()
-cli.nextDev(['-p', process.env.PORT || '3000'])
+dotenv.config();
+const dev = process.env.NODE_ENV !== "production";
+const port = parseInt(process.env.PORT || "3000", 10);
+
+const app = next({ dev, port });
+
+app.prepare().then(() => {
+  const httpServer = createServer();
+
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+});
+
