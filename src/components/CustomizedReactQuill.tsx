6bca8760415914dbbd136d2c3c331cc90f97eda4
momingse <da1710080850@gmail.com>
2024-09-17 10:51:57 +0800

    add: change quill styling

--- src/components/CustomizedReactQuill.tsx
+++ src/components/CustomizedReactQuill.tsx
@@ -3,6 +3,7 @@
 import { FC, useMemo } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import "../app/quill.css"
 
 type CustomizedReactQuillProps = {
   theme: string;
