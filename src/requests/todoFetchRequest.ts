e3b81c461efdc7eabc574e7f68755aa6d6fe29bb
momingse <da1710080850@gmail.com>
2025-05-01 11:55:31 +0800

    fix: infinite loading when not login

--- src/requests/todoFetchRequest.ts
+++ src/requests/todoFetchRequest.ts
@@ -1,11 +1,15 @@
 import { axiosInstance } from "@/lib/axios";
 import { Todo } from "@prisma/client";
+import { AxiosError } from "axios";
 
 const todoFetchRequest = async () => {
   try {
     const result = await axiosInstance.get("/todo");
     return result.data as Promise<Todo[]>;
   } catch (error) {
+    if (error instanceof AxiosError && error.response?.status === 401) {
+      return [];
+    }
     throw error;
   }
 };
